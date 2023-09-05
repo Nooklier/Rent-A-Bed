6b53f8bc0e077e1f6ced31c3db3a7416ccf51dc4
Nooklier <muenseepromn@gmail.com>
2023-09-04 23:11:19 -0700

    Add error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,9 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
+const routes = require('./routes');
+
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
@@ -38,12 +41,42 @@ if (!isProduction) {
     })
   );
 
-/************************************************************************************************************************************************/
-
-const routes = require('./routes');
-
-// ...
+/*************************************************************** Errors Middleware ***************************************************************/
 
 app.use(routes);                                                               // Connect all the routes
+
+app.use((_req, _res, next) => {                                                // Catch unhandled requests and forward to error handler.
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = { message: "The requested resource couldn't be found." };
+    err.status = 404;
+    next(err);
+  });
+
+
+app.use((err, _req, _res, next) => {                                           // Process sequelize errors
+  
+    if (err instanceof ValidationError) {                                      // check if error is a Sequelize error:
+      let errors = {};
+      for (let error of err.errors) {
+        errors[error.path] = error.message;
+      }
+      err.title = 'Validation error';
+      err.errors = errors;
+    }
+    next(err);
+  });
+
+ 
+app.use((err, _req, res, _next) => {                                           // Error formatter
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
+    });
+  });
 
 module.exports = app;                                                          // Always at the bottom of app.js
