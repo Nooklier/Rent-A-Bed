466ec1b18aac0f83271b0c61b459c550faf6880d
Nooklier <muenseepromn@gmail.com>
2023-09-11 23:08:27 -0700

    Add a backend endpoint to get the current user session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -35,6 +35,8 @@ router.post(
         id: user.id,
         email: user.email,
         username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName
       };
   
       await setTokenCookie(res, safeUser);
@@ -52,6 +54,27 @@ router.delete(
     (_req, res) => {
       res.clearCookie('token');
       return res.json({ message: 'success' });
+    }
+  );
+
+/****************************************************************** Restore session user *****************************************************************/
+
+router.get(
+    '/',
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        const safeUser = {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+          firstName: user.firstName,
+          lastName: user.lastName
+        };
+        return res.json({
+          user: safeUser
+        });
+      } else return res.json({ user: null });
     }
   );
 
