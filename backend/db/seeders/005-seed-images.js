33d3ce6f524f02ce897737baf9a590680d2bd0f1
Nooklier <muenseepromn@gmail.com>
2023-11-29 17:18:00 -0800

    seeder finished

--- backend/db/seeders/005-seed-images.js
+++ backend/db/seeders/005-seed-images.js
@@ -2,20 +2,23 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Images', [
-      {
-        url: 'https://res.cloudinary.com/dikyl7t9p/image/upload/v1701302677/283163011_tij7ks.jpg',
-        preview: true,
-        imageableId: 1,
-        imageableType: 'Spot'
-      },
-      {
-        url: 'https://res.cloudinary.com/dikyl7t9p/image/upload/v1701303073/The-Venetian-Las-Vegas-Outdoor-area-1_fa2qdx.jpg',
-        preview: true,
-        imageableId: 1,
-        imageableType: 'Review'
-      }
-    ])
+    try {
+
+      await queryInterface.bulkInsert('Images', [
+        {
+          url: 'https://res.cloudinary.com/dikyl7t9p/image/upload/v1701302677/283163011_tij7ks.jpg',
+          preview: true,
+          imageableId: 1,
+          imageableType: 'Spot'
+        },
+        // {
+        //   url: 'https://res.cloudinary.com/dikyl7t9p/image/upload/v1701303073/The-Venetian-Las-Vegas-Outdoor-area-1_fa2qdx.jpg',
+        //   preview: true,
+        //   imageableId: 1,
+        //   imageableType: 'Review'
+        // }
+      ])
+    } catch(e) {console.log(e)}
   },
 
   async down (queryInterface, Sequelize) {
