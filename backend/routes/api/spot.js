544bab8595c3ecf4869931ccc77e17b9857cbe71
Nooklier <muenseepromn@gmail.com>
2023-12-04 00:44:43 -0800

    get all spots finished

--- backend/routes/api/spot.js
+++ backend/routes/api/spot.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const {User, Spot, Booking, Image, Review} = require('../../db/models');
 
+/************************************************ GET ALL SPOTS *********************************************/
 
-router.get('spots', async (req, res) => {
-    const allSpots = Spot.findAll({
-        attributes: {
-            exclude : [ 'updatedAt', 'createdAt']
-        }
-    })
+router.get('', async (req, res) => {
+    const allSpots = await Spot.findAll({})
+
+    return res.status(200).json(allSpots)
 })
+
+module.exports = router;
