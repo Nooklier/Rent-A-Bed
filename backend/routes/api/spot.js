0c065136be01e755e9124336847edea6ba37541c
Nooklier <muenseepromn@gmail.com>
2023-12-04 01:37:33 -0800

    get all spots by current user done

--- backend/routes/api/spot.js
+++ backend/routes/api/spot.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {User, Spot, Booking, Image, Review} = require('../../db/models');
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
 
 /************************************************ GET ALL SPOTS *********************************************/
 
@@ -9,5 +10,21 @@ router.get('', async (req, res) => {
 
     return res.status(200).json(allSpots)
 })
+
+/************************************** GET ALL SPOTS BY CURRENT USER ***************************************/
+
+router.get('/current', requireAuth, async (req, res) => {
+
+    const currentUser = await User.findByPk(req.user.id)
+
+    const currentUserSpot = await Spot.findAll({
+        where: {
+            ownerId : currentUser.id
+        }
+    })
+
+    res.status(200).json({"Spots": currentUserSpot})
+})
+
 
 module.exports = router;
