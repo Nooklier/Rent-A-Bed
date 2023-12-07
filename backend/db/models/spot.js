04b918bb24b4ac716623f4de6c6a36152014cc32
Nooklier <muenseepromn@gmail.com>
2023-12-06 17:12:32 -0800

    create spot & add image to spot done

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -46,30 +46,50 @@ module.exports = (sequelize, DataTypes) => {
     },
     address: {
       type: DataTypes.STRING,
+      allowNull: false
     },
     city: {
       type: DataTypes.STRING,
+      allowNull: false
     },
     state: {
       type: DataTypes.STRING,
+      allowNull: false
     },
     country: {
       type: DataTypes.STRING,
+      allowNull: false
     },
     lat: {
       type: DataTypes.DECIMAL,
+      validate: {
+        isDecimal: true,
+        min: -90,
+        max: 90
+      }
     },
     lng: {
       type: DataTypes.DECIMAL,
+      validate: {
+        min: -180,
+        max: 180
+      }
     },
     name: {
       type: DataTypes.STRING,
+      validate: {
+        len: [0,50]
+      }
     },
     description: {
       type: DataTypes.STRING,
+      allowNull: false
     },
     price: {
       type: DataTypes.DECIMAL,
+      validate: {
+        min: 0
+      }
     },
   }, {
     sequelize,
--- backend/routes/api/spot.js
+++ backend/routes/api/spot.js
@@ -2,6 +2,50 @@ const express = require('express');
 const router = express.Router();
 const {User, Spot, Booking, Image, Review} = require('../../db/models');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+/************************************************ VALIDATIONS ***********************************************/
+
+const validateSpot = [
+    check('address')
+        .exists({checkFalsy: true})
+        .withMessage("Street address is required"),
+    check("city")
+        .exists({checkFalsy: true})
+        .withMessage("City is required"),
+    check("state")
+        .exists({checkFalsy: true})
+        .withMessage("State is required"),
+    check("country")
+        .exists({checkFalsy: true})
+        .withMessage("Country is required"),
+    check("lat")
+        .exists({checkFalsy: true})
+        .isFloat({min: -90, max: 90})
+        .withMessage("Latitude must be within -90 and 90"),
+    check("lng")
+        .exists({checkFalsy: true})
+        .isFloat({min: -180, max: 180})
+        .withMessage("Longitude must be within -180 and 180"),
+    check("name")
+        .exists({checkFalsy: true})
+        .withMessage("Name is required"),
+    check("name")
+        .isLength({max: 50})
+        .withMessage("Name must be less than 50 characters"),
+    check("description")
+        .exists({checkFalsy: true})
+        .withMessage("Description is required"),
+    check("price")
+        .exists({checkFalsy: true})
+        .withMessage("Price per day must be a positive number"),
+    check("price")
+        .isFloat({min: 0})
+        .withMessage("Price per day must be a positive number"),
+        handleValidationErrors
+
+]        
 
 /************************************************ GET ALL SPOTS *********************************************/
 
@@ -144,7 +188,49 @@ router.get('/:spotId', async (req, res) => {
     res.json(spotDetails)
 })
 
-/************************************ GET DETAILS OF A SPOT FROM AN ID ***************************************/
+/************************************************ CREATE SPOT  **********************************************/
+
+router.post('/', requireAuth, validateSpot, async (req, res, next) => {
+
+    const {ownerId, address, city, state, country, lat, lng, name, description, price} = req.body;
+    const newSpot = await Spot.create(req.body)
+
+    res.status(201).json(newSpot)
+
+})
+
+/******************************** ADD AN IMAGE TO A SPOT BASE ON SPOT'S ID  *********************************/
+
+router.post('/:spotId/images', requireAuth, async (req, res) => {
+
+    const { spotId } = req.params;
+    const { url, preview} = req.body;
+
+    const spot = await Spot.findOne({ where: { id : spotId }})
+
+    if (!spot) {
+        res.status(404).json({
+            "message": "Spot couldn't be found"
+        })
+    }
+
+    const newImage = await Image.create({
+        url : url,
+        preview: preview,
+        imageableId: spotId,
+        imageableType: "Spot"
+    })
+
+    res.status(200).json({
+        id: newImage.id,
+        url: newImage.url,
+        preview: newImage.preview
+    })
+
+})
+
+
+
 
 
 module.exports = router;
