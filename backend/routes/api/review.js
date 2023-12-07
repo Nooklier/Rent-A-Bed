af0ac78a0f78f606857429d0f80a97ee9f9f3fb9
Nooklier <muenseepromn@gmail.com>
2023-12-07 04:04:30 -0800

    edit review done

--- backend/routes/api/review.js
+++ backend/routes/api/review.js
@@ -5,6 +5,21 @@ const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
+/************************************************ VALIDATIONS ***********************************************/
+
+const validateReview = [
+    check('review')
+        .exists({checkFalsy: true})
+        .withMessage("Review text is required"),
+    check('stars')
+        .exists({checkFalsy: true})
+        .withMessage("Stars must be an integer from 1 to 5"),
+    check('stars')
+        .isFloat({min: 1 , max: 5})
+        .withMessage("Stars must be an integer from 1 to 5"),
+    handleValidationErrors
+]
+
 /************************************************ GET ALL REVIEWS OF CURRENT USER *********************************************/
 
 router.get('/current', requireAuth, async (req, res) => {
@@ -58,7 +73,7 @@ router.get('/current', requireAuth, async (req, res) => {
     
 })
 
-/****************************************** ADD AN IMAGE TO A REVIEW BASE ON REVIEW'S ID ************************************************/
+/****************************************** ADD AN IMAGE TO A REVIEW BASE ON REVIEW'S ID ********************************************/
 
 router.post('/:reviewId/images', requireAuth, async (req, res) => {
 
@@ -100,8 +115,38 @@ router.post('/:reviewId/images', requireAuth, async (req, res) => {
     })
 })
 
+/**************************************************** EDIT A REVIEW ************************************************************/
 
+router.put('/:reviewId', requireAuth, validateReview, async (req, res) => {
 
+    const { reviewId } = req.params;
+    const { review, stars} = req.body;
+
+    let currentReview = await Review.findByPk(reviewId);
+
+    if (!currentReview) {
+        return res.status(404).json({
+            "message": "Review couldn't be found"
+        })
+    }
+    
+    await currentReview.update({
+        review: review,
+        stars: stars
+    })
+
+    let updatedReview = {
+        id: reviewId,
+        userId: req.user.id,
+        spotId: currentReview.spotId,
+        review: currentReview.review,
+        stars: currentReview.stars,
+        createdAt: currentReview.createdAt,
+        updatedat: currentReview.updatedAt
+    }
+
+    res.status(200).json(updatedReview)
+})
 
 
 
