5c5e77f19bd87c18b24fb09f1e988255c7821cc0
Nooklier <muenseepromn@gmail.com>
2024-01-16 09:27:10 -0800

    logformpage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './store/store.js';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
 
 // Create a variable to access your store and expose it on the window
 const store = configureStore();
@@ -13,11 +14,12 @@ if (process.env.NODE_ENV !== 'production') {
   window.store = store;
 }
 
-if (import.meta.env.MODE !== 'production') {
+if (import.meta.env.MODE !== "production") {
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions; // <-- ADD THIS LINE
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
