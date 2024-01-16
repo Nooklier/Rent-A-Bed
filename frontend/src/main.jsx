a8a7faf84fbb09444087ae526bb9550fac116a60
Nooklier <muenseepromn@gmail.com>
2024-01-16 06:42:48 -0800

    uh idk

---
+++ frontend/src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import { Provider } from 'react-redux';
+import configureStore from './store';
+
+// Create a variable to access your store and expose it on the window
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
